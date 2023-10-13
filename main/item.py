f3481f9679f6ce8a5963af24095d75fc43493097
monicaa37 <monikadamanik37@gmail.com>
2023-10-13 11:52:09 +0700

    tugas 6 finish

--- main/item.py
+++ main/item.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
-    amount = models.IntegerField()
+    amount = models.IntegerField(default=1)
     price = models.IntegerField()
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
